d5c63bdf4e5ec71675539490f16bf0efa0b2a79a
vovantung <vovantungdt123@gmail.com>
2024-07-21 22:09:23 +0700

    cm14

--- src/main/java/com/example/app2/Api.java
+++ src/main/java/com/example/app2/Api.java
@@ -43,7 +43,7 @@ public class Api {
             throw new TxException("An error occurred while processing your request");
         }
 
-        return "APP2: " + responseEntity.getBody();
+        return "Test: " + responseEntity.getBody();
 
     }
 
